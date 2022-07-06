6500313e58e05eac8ecf735aae3965fb7092680a
ethanvannest <ethan3vannest@gmail.com>
2022-07-06 17:23:17 -0600

    ability to name pet

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// console.log('this works')
+let petName = prompt('What is your pets name?')
+if (petName === null) {
+    petName = 'The best pet'
+}
+// console.log(petName)
+
+class game {
+    constructor(){
+
+        
+    }
+}
+class tam {
+    constructor(){
+
+    }
+}
+//add ability to name pet
+//add a hunger, sleepiness, boredom, and age(set time interval to age every 15 seconds)
+//button to feed pet, turn screen to dark mode, play with pet
+//display a character on screen
